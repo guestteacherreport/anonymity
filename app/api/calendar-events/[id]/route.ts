c156fd0a159b1e89525cc32bc102ce1bff94cb3a
accesstbilq <accesstbilq@gmail.com>
2026-07-07 17:09:40 +0530

    made changes for calendar

--- app/api/calendar-events/[id]/route.ts
+++ app/api/calendar-events/[id]/route.ts
@@ -43,7 +43,7 @@ export async function GET(req: NextRequest, { params }: { params: Promise<{ id: 
   }
 }
 
-export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+export async function PUT(req: NextRequest, { params }:  { params: Promise<{ id: string }> }) {
   try {
     const session = await getServerSession(authOptions);
 
